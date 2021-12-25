b08f93492c2192917cbef51da3e0301be3baf8dc
Abdulaziz <abdu3332099@outlook.com>
2021-12-25 13:13:53 +0500

    gg

---
+++ RegBot/Data.cs
@@ -0,0 +1,40 @@
+﻿namespace RegBot
+{
+    
+    public class Data
+    {
+        public class Dict
+        {
+            public Guid Id;
+            public string text;
+            public DateTime created;
+        }
+
+        List<Dict> data = new List<Dict>();
+        public void Add(Dict dict)
+        {
+            data.Add(dict);
+            Clear();
+        }
+        public Dict GetDict(Guid id)
+        {
+            return data.FirstOrDefault(p => p.Id == id);
+        }
+        public void Del(Dict id)
+        {
+            Clear();
+            data.Remove(id);
+        }
+
+        void Clear()
+        {
+            foreach (var item in data)
+            {
+                if (DateTime.Now - item.created > TimeSpan.FromMinutes(10))
+                {
+                    data.Remove(item);
+                }
+            }
+        }
+    }
+}
